005a777fca063fa26f97a94eac2e00f43bfa4798
Bob Schmertz <rschmertz@yahoo.com>
2016-10-02 00:50:20 -0400

    Typo in server.sh

--- server.sh
+++ server.sh
@@ -13,7 +13,7 @@ if [ -r $ENV_FILE ]; then
     . $ENV_FILE
 else
     echo $ENV_FILE not found
-    echo Please place $(basename $ENV_FILE) in $(dirname $ENV_fILE)
+    echo Please place $(basename $ENV_FILE) in $(dirname $ENV_FILE)
     echo or edit this script to indicate the proper location of this file
     exit 1
 fi
