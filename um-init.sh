3ee7b782715546916266918902cc9ea18cce4a59
Bob Schmertz <rschmertz@yahoo.com>
2017-07-15 23:28:47 -0400

    script sorta working for start when called directly

--- um-init.sh
+++ um-init.sh
@@ -5,20 +5,31 @@
 # chkconfig: 35 79 14
 # description: Lean35 user/group management and notification system
 
-. /etc/rc.d/init.d/functions
+#. /etc/rc.d/init.d/functions
+# . /lib/lsb/init-functions
 
 
 # NOTE: if you change any OPTIONS here, you get what you pay for:
 # this script assumes all options are in the config file.
 UM_USER=usersmanagement
+NAME=usersmanagement
 UM_ID=$(id -u $UM_USER)
-COMMAND_NAME=node
-SERVER_PATH=/home/usersmanagement/dist/server
+USER_HOME=/home/usersmanagement/users-management
+PATH=/home/usersmanagement/.nvm/versions/node/v0.12.18/bin:$PATH
+COMMAND_NAME=`which node`
+echo node is $COMMAND_NAME
+SERVER_PATH=$USER_HOME/dist/server
+COMMAND_OPTS="$SERVER_PATH/users-management.js"
 export SENDGRID_FROMEMAIL_NAME="Maryland Community Band"
 export NODE_ENV=production
 
+RUNDIR=$USER_HOME
+PIDFILE=$RUNDIR/$NAME.pid
+
 if [ -r /home/usersmanagement/env.sh ]; then
     . /home/usersmanagement/env.sh
+else
+    echo env file not found
 fi
 
 getpid()
@@ -30,7 +41,14 @@ start()
 {
   echo Starting users-management
   #daemon --user "$UM_USER" "node $SERVER_PATH/users-management.js >/dev/null 2>&1"
-  runuser $UM_USER -c "node $SERVER_PATH/users-management.js >/dev/null 2>&1&"
+  #runuser $UM_USER -c "node $SERVER_PATH/users-management.js >/dev/null 2>&1&"
+  # Start the process using the wrapper (copied from mongod script)
+  : start-stop-daemon --background --start --pidfile $PIDFILE \
+                    --make-pidfile --chuid $UM_USER \
+                    --exec $COMMAND_NAME $COMMAND_OPTS prod
+  # Because suitable things weren't working
+  cd $USER_HOME
+  su $UM_USER -c "$COMMAND_NAME $COMMAND_OPTS > $SERVER_PATH/mcb-mgmt.log 2>$SERVER_PATH/mcb-mgmt.err"
   RETVAL=$?
   echo
   #[ $RETVAL -eq 0 ] && touch /var/lock/subsys/mongod
