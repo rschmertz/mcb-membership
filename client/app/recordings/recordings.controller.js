1167122a9383f16e43fa6dcdc549d01d1dd56969
Bob Schmertz <rschmertz@yahoo.com>
2018-07-29 20:25:19 -0400

    Finish(?) recordings page

--- client/app/recordings/recordings.controller.js
+++ client/app/recordings/recordings.controller.js
@@ -3,4 +3,22 @@
 angular.module('usersManagementApp')
   .controller('RecordingsCtrl', function ($scope) {
       this.message = 'Hello';
+      this.kscope2017files = [
+	  {
+	      fileName: "1 Valdres March - Hanssen.mp3",
+	      displayName: "Valdres",
+	  },
+	  {
+	      fileName: "2 Psalm 46 - Zdechlik.mp3",
+	      displayName: "Psalm 46",
+	  },
+	  {
+	      fileName: "3 Handel in the Strand - Grainger.mp3",
+	      displayName: "Handel in the Strand",
+	  },
+	  {
+	      fileName: "4 Toccata - Frescobaldi.mp3",
+	      displayName: "Toccata",
+	  },
+      ];
   });
